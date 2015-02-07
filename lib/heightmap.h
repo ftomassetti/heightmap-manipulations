51a076cf6476674ddce04962e3d7e4f4ddc6e9d8
Federico Tomassetti <federico@tomassetti.me>
2015-02-07 11:05:22 +0000

    Adding example of PNG generation

---
+++ lib/heightmap.h
@@ -0,0 +1,16 @@
+#ifndef HEIGHTMAP_H
+#define HEIGHTMAP_H
+
+typedef struct _Heightmap {
+    unsigned int width;
+    unsigned int height;
+    unsigned int* data;
+} Heightmap;
+
+Heightmap create(unsigned int width, unsigned int height){
+
+}
+
+
+
+#endif // HEIGHTMAP_H
